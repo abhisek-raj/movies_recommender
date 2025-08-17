2282f06383e3a493930e41e7d75fcca1fdab502b
Abhisek raj tiwary <141418987+abhisek-raj@users.noreply.github.com>
2025-08-17 22:36:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ setup(
     version="1.0.0",
     packages=find_packages(),
     install_requires=[
-        'streamlit==1.32.0',
-        'pandas==2.1.0',
-        'numpy==1.26.0',
-        'scikit-learn==1.3.0',
+        'streamlit',
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'gdown',
     ],
     python_requires='>=3.8',
     author="Avisek",
     description="A movie recommendation system built with Streamlit",
     url="https://github.com/yourusername/movies_recommender"
 )
+
